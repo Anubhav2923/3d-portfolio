47340dd0112d7c51e09477685671ba11d8ea09e1
Anubhav singh <93425405+Anubhav2923@users.noreply.github.com>
2025-01-12 23:09:22 +0530

    skill section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,14 +2,24 @@ import { BallCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
 
+import './styleCanva.css';
+
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className='position-relative h-[50vh]'>
+      <div className='heading-text-div'>
+        <h1
+          className='heading-text-tech'
+        >I Develop Cutting-Edge Web Applications <br></br> Using Latest Technologies</h1>
+      </div>
+      <div className='flex flex-row flex-wrap justify-center gap-10 mt-20'>
+      
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
+    </div>
     </div>
   );
 };
